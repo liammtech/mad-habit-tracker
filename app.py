0dbf160d39b032856f22ff9ee414844196bfe593
liam <liammtech@gmail.com>
2026-03-04 19:34:29 +0000

    Add bare-bones root app route

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template()
